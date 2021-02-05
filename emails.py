4a9ff082731bd036f3504baa8d20d909e78cde93
Brandt <bwinklerprins@yahoo.com>
2021-02-05 11:09:25 -0800

    made things a bit nbetter)

--- emails.py
+++ emails.py
@@ -1,6 +1,6 @@
-firstName = "chad"
-lastName = "doerge"
-company = "aiera.com"
+firstName = input("Enter lead firstname: ")
+lastName = input("Enter lead lastname: ")
+company = input("Enter company url (omit https:// just domainName.TLD): ")
 
 usernames = [firstName[0] + lastName, firstName, lastName, firstName + lastName, firstName + '.' + lastName, firstName[0] + '.' + lastName, firstName[0] + lastName[0], firstName[0] + '.' + lastName[0],  firstName + lastName[0], firstName + '.' + lastName[0], lastName + '.' + firstName, lastName + firstName]
 emails = [username + "@" + company for username in usernames]
